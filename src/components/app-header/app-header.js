2a2f08e59443845977823c47e6cb61a0feee7626
Askhat <s.askhat.b@gmail.com>
2020-08-16 22:32:32 +0600

    Initial commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./app-header.css";
+
+const AppHeader = () => {
+  return (
+    <div className="app-header">
+      <h1>Асхат Сулейменов</h1>
+      <h2>5 записей, из них понравились 1</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
